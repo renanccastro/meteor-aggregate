383818aec6f5e4481618774fa41b4118a53f5d17
lukas strassel <lukasstrassel@googlemail.com>
2018-05-29 13:28:25 +0200

    fix context issues

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ Mongo.Collection.prototype.aggregate = function(pipelines, options) {
     // >= Meteor 1.0.4
     coll = this.rawCollection();
   } else {
-	// < Meteor 1.0.4
+    // < Meteor 1.0.4
     coll = this._getCollection();
   }
-  if(MongoInternals.NpmModules.mongodb.version[0] === '3') {
-    return (wrapAsync(coll.aggregate.bind(coll))(pipelines, options)).toArray();
+  if (MongoInternals.NpmModules.mongodb.version[0] === '3') {
+    const cursor = wrapAsync(coll.aggregate, coll)(pipelines, options);
+    const value = wrapAsync(cursor.toArray, cursor)();
+    return value;
   }
   return wrapAsync(coll.aggregate.bind(coll))(pipelines, options);
-}
-
+};
